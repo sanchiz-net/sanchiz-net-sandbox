18f45180a759e8c99f5e3eee9be26ebadc275b1a
Alexander Schedrov <alexander.schedrov@gmail.com>
2014-08-30 16:40:23 +0300

    Added example_template.

---
+++ example_template/custom_template.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Custom template.
+ */
+?>
+
+<div class="template-custom-template-wrapped">
+  <div class="body">
+    <?php print $body; ?>
+  </div>
+  <div class="title">
+    <?php print $title; ?>
+  </div>
+</div>
